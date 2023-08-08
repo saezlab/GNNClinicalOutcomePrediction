230e5e0dbda77d7e0895324638e88b7dcea5e56d
Deniz Germen <73364909+dGermen@users.noreply.github.com>
2023-08-08 12:51:24 +0300

    Plotting, correlation calculation

--- bin/plotting.py
+++ bin/plotting.py
@@ -429,3 +429,52 @@ def plot_age_vs_survibility(fl_name="age_vs_survibility"):
     plt.savefig(os.path.join(PLOT_PATH, "manuscript_figures", f"{fl_name}.pdf"))
 
 # plot_age_vs_survibility(fl_name="age_vs_survibility")
+
+# Visualization functions kept here
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import custom_tools
+
+def visualize_clinical_data(c_data, s_c_data, clinical_type_column_name = "clinical_type"):
+    c_data.columns = c_data.columns.str.strip()
+
+    # Print columns
+    print("Clinical data columns: ", c_data.columns)
+    print("Single cell data columns: ", s_c_data.columns)
+
+    # Keep rows in c_data with PIDs in single_cell_data
+    c_data = c_data[c_data["Patient ID"].isin(s_c_data["metabricId"])]
+
+    if clinical_type_column_name not in c_data.columns:
+        c_data = custom_tools.type_processor(c_data)
+
+    # Define custom order for the plot
+    custom_order = ["TripleNeg", "HR-HER2+", "HR+HER2-", "HR+HER2+", "HER2-NAN"]
+
+    # Clinical_type vs Survival candle plot using sns and order the clinical_type
+    clinical_type = c_data["clinical_type"]
+    survival = c_data["Overall Survival (Months)"]
+    ax = sns.boxplot(x=clinical_type, y=survival, order=custom_order)
+    ax.set(xlabel="Clinical Type", ylabel="Overall Survival (Months)")
+    plt.show()
+
+    clinical_type = c_data["clinical_type"]
+    age = c_data["Age at Diagnosis"]
+    ax = sns.boxplot(x=clinical_type, y=age, order=custom_order)
+    ax.set(xlabel="Clinical Type", ylabel="Age at Diagnosis")
+    plt.show()
+
+    
+    # Age vs Survival scatter plot,
+
+    
+    ax = sns.scatterplot(x=age, y=survival, hue=clinical_type, hue_order=custom_order)
+    ax.set(xlabel="Age at Diagnosis", ylabel="Overall Survival (Months)")
+    plt.show()
+
+    
+    # HER2-NAN count
+    print("HER2-NAN count: ", len(c_data[c_data["clinical_type"] == "HER2-NAN"]))
+    # Total count   
+    print("Total count: ", len(c_data))
